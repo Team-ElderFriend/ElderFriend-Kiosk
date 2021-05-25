c57db764103c1ed5eb540195aed0ac78f2b7d196
Akkanit <55478577+Akkanit@users.noreply.github.com>
2021-05-25 17:10:25 +0700

    change selectedMenus from Object to Array
    For showing selected menus by ordering order

--- src/reducers/counters.js
+++ src/reducers/counters.js
@@ -1,32 +1,30 @@
-export default (state = {sum:0, selectedMenus:{}}, action) => {
+export default (state = {sum:0, selectedMenus:[]}, action) => {
   let nstate = {
     ...state
 };
+  let inde = nstate.selectedMenus.findIndex((element) => element.id === action.id);
     switch (action.type) {
       case 'ADD':
         console.log(action);
-        if(action.id in state.selectedMenus)
-        nstate.selectedMenus[action.id].number = nstate.selectedMenus[action.id].number + 1;
+        if(inde > -1)
+          nstate.selectedMenus[inde].number = nstate.selectedMenus[inde].number + 1;
         else{
-            nstate.selectedMenus[action.id] = {number:1, cost: action.cost}
-            //nstate.selectedMenus[action.id].number = 1;
-            //nstate.selectedMenus[action.id].cost = action.cost;
+            nstate.selectedMenus.push({id: action.id, number:1, cost: action.cost})
         }
         nstate.sum = nstate.sum + action.cost;
         return nstate;
       case 'REDUCE':
-        nstate.sum = nstate.sum - nstate.selectedMenus[action.id].cost;
-
-        if(nstate.selectedMenus[action.id].number==1){
-            delete nstate.selectedMenus[action.id];
+        nstate.sum = nstate.sum - nstate.selectedMenus[inde].cost;
+        if(nstate.selectedMenus[inde].number==1){
+          nstate.selectedMenus.splice(inde, 1);
         }
         else{
-            nstate.selectedMenus[action.id].number = nstate.selectedMenus[action.id].number - 1;
+            nstate.selectedMenus[inde].number = nstate.selectedMenus[inde].number - 1;
         }
         return nstate;
       case 'REMOVE':
-        nstate.sum = nstate.sum - nstate.selectedMenus[action.id].number * nstate.selectedMenus[action.id].cost;
-        delete nstate.selectedMenus[action.id];
+        nstate.sum = nstate.sum - nstate.selectedMenus[inde].number * nstate.selectedMenus[inde].cost;
+        nstate.selectedMenus.splice(inde, 1);
         return nstate
       default:
         return state
