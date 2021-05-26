4eb1fe51ae9a5e6008be65df66fa0f4b48760676
jaewoo.kim <jaewoo986@gmail.com>
2021-05-26 12:47:10 +0900

    Change size of menu view element according to the zoom level

--- src/components/MenuListView/MenuListView.js
+++ src/components/MenuListView/MenuListView.js
@@ -2,27 +2,35 @@ import React from 'react'
 import classNames from 'classnames/bind'
 import styles from "./MenuListView.scss"
 import MenuViewElement from "../MenuViewElement";
+import ZoomContext from "../ZoomContext";
 
 const cx = classNames.bind(styles);
 
 
 const MenuListView = ({menuIds, ...props}) => {
     return (
-        <div className={cx('menu-list-view')} {...props}>
-            <div className={cx('inner-box')}>
-                {
-                    menuIds.map((menuId) => {
-                        return (
-                            <div className={cx('slot')}>
-                                <MenuViewElement menuId={menuId}/>
-                            </div>
-                        )
-                    })
+        <ZoomContext.Consumer>
+            {
+                ({zoom_scale}) => {
+                    return <div className={cx('menu-list-view')} {...props}>
+                        <div className={cx('inner-box')}>
+                            {
+                                menuIds.map((menuId) => {
+                                    return (
+                                        <div className={cx('slot')}
+                                             style={{margin: (12 * zoom_scale + 'px') + ' ' + (9 * zoom_scale + 'px'),}}>
+                                            <MenuViewElement menuId={menuId}/>
+                                        </div>
+                                    )
+                                })
+                            }
+                                </div>
+                                </div>
+                            }
                 }
-            </div>
-        </div>
-    );
-}
-;
+                </ZoomContext.Consumer>
+                );
+                }
+                ;
 
-export default MenuListView
+                export default MenuListView
